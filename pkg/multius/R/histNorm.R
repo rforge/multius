539ea55801566996b1a295eee90fdd36cf1bb038
cugmas_990 <cugmas_990@f465408c-3b6e-4e74-ba7b-223a1f180679>
2020-02-20 13:51:26 +0000

    Posodobljene so help datoteke. Dodana je funkcija histNorm.R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multius@18 f465408c-3b6e-4e74-ba7b-223a1f180679

---
+++ pkg/multius/R/histNorm.R
@@ -0,0 +1,27 @@
+#' Histogram with normal curve
+#'
+#' @description The function draws a histogram with a normal density curve. The parameters (mean and standard deviation) are estimated on the empirical data.
+#' @param y A vector of observations.
+#' @param freq Wheter frequencies (\code{freq = TRUE}) of density (\code{freq = FALSE}) should be represented on \eqn{y}-axis.
+#' @param breaks See help file for function \code{hist}.
+#' @param ... Arguments passed to function \code{hist}.
+#' @examples
+#' histNorm(rnorm(1000), freq = TRUE)
+#' histNorm(rnorm(1000), freq = FALSE)
+#' @author Ales Ziberna
+
+histNorm <- function(y, breaks = "Sturges", freq = TRUE, ...){
+  y <- na.omit(y)
+  hist(y,
+       breaks = breaks,
+       freq = freq, ...)
+
+  if (length(breaks) == 1) {
+    if (breaks == "Sturges") {nbars <- nclass.Sturges(y)} else nbars <- breaks
+  } else nbars <- length(breaks) - 1
+
+  curve(dnorm(x,
+              mean = mean(y),
+              sd = sd(y))*ifelse(freq == TRUE, yes = length(!is.na(y))*diff(range(y))/nbars, no = 1),
+        add = TRUE, col = "red", lwd = 2, lty = 2, xpd = TRUE)
+}
