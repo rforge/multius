e9ef777d52d478ce8a964ca6b9c140acc5e0886a
cugmas_990 <cugmas_990@f465408c-3b6e-4e74-ba7b-223a1f180679>
2020-09-11 11:47:31 +0000

    Dopolnjena dokumentacija za funkcijo report.ttest.R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multius@32 f465408c-3b6e-4e74-ba7b-223a1f180679

--- pkg/reporter/R/report.ttest.R
+++ pkg/reporter/R/report.ttest.R
@@ -4,8 +4,12 @@
 #' @param ordinalna The name of categorical variable. One variable can be considered.
 #' @param intervalna The name of interval/numberic variable(s). A string or a list. Several numeric variables can be considered.
 #' @param dataset The dataframe with categorical and numerical variables.
+#' @param hedgesG Should Hedges' g statistics be computed?
+#' @note Hedges' g statistics is a variation of Cohen's d, corrected for bias due to the sammple size.
 #' @examples
 #' report.ttest(ordinalna = "vs", intervalna = c("disp", "wt", "qsec"), dataset = mtcars)
+#' @references
+#' Hedges, L., & Olkin, I. (2014). Statistical methods for meta-analysis. Academic press.
 #' @author Marjan Cugmas
 #' @export
 
