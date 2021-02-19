3f2496d48b8d9680da4445f88142ecd95aad0e38
cugmas_990 <cugmas_990@f465408c-3b6e-4e74-ba7b-223a1f180679>
2021-02-19 13:36:42 +0000

    Manjši popravki v funkciji freqTab.R (privzeto dve decimalni mesti, možnost cum za brez kumulativnih deležev in odstotkov) ter histNorm.R (popravek za breaks).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multius@49 f465408c-3b6e-4e74-ba7b-223a1f180679

--- pkg/multius/R/freqTab.R
+++ pkg/multius/R/freqTab.R
@@ -3,20 +3,23 @@
 #' @description The function creates a frequency table with percentages for the selected categorical variable.
 #' @param x Vector with the values of a categorical variable.
 #' @param dec Number of decimal places for percentages.
+#' @param cum wheter to calculate cummulative frequencies and percentages (default \code{TRUE}).
 #' @param \dots Arguments passed to function \code{table}.
 #' @examples
 #' freqTab(mtcars[,2], dec = 1)
 #' @author Aleš Žiberna
 
-freqTab<-function(x, dec=NULL, ...){
-  tbl<-table(x,...)
-  cumFreq<-cumsum(tbl)
-  perc<-tbl/sum(tbl)*100
-  cumPerc<-cumsum(perc)
-  if(!is.null(dec)){
-    perc<-round(perc,dec)
-    cumPerc<-round(cumPerc,dec)
+freqTab <- function (x, dec = 2, cum = TRUE, ...){
+  tbl <- table(x, ...)
+  if (cum)  cumFreq <- cumsum(tbl)
+  perc <- tbl/sum(tbl) * 100
+  if (cum) cumPerc <- cumsum(perc)
+  if (!is.null(dec)) {
+    perc <- round(perc, dec)
+    if (cum) cumPerc <- round(cumPerc, dec)
   }
-  frekTab<-as.data.frame(cbind("Freq."=tbl,"Cum. freq."=cumFreq, "%"=perc,"Cum. %" = cumPerc))
+
+  if (cum) frekTab <- as.data.frame(cbind(Freq. = tbl, `Cum. freq.` = cumFreq, `%` = perc, `Cum. %` = cumPerc))
+  if (!cum) frekTab <- as.data.frame(cbind(Freq. = tbl, `%` = perc))
   return(frekTab)
 }
--- pkg/multius/R/histNorm.R
+++ pkg/multius/R/histNorm.R
@@ -4,16 +4,20 @@
 #' @param y A vector of observations.
 #' @param freq Wheter frequencies (\code{freq = TRUE}) of density (\code{freq = FALSE}) should be represented on \eqn{y}-axis.
 #' @param breaks See help file for function \code{hist}.
-#' @param ... Arguments passed to function \code{hist}.
+#' @param \dots Arguments passed to function \code{hist}.
 #' @examples
 #' histNorm(rnorm(1000), freq = TRUE)
 #' histNorm(rnorm(1000), freq = FALSE)
 #' @author Marjan Cugmas
 
-histNorm <- function(y, breaks = "Sturges", freq = TRUE, ...){
+histNorm <- function (y, breaks = "Sturges", freq = TRUE, ...) {
   y <- na.omit(y)
-  h<-hist(y, freq = freq, ...)
-  if(!h$equidist & freq) {
+  h <- hist(y, freq = freq, breaks, ...)
+  if (!h$equidist & freq) {
     warning("Normal distribution not added due to using unequal bin widths with frequencies!")
-  } else curve(dnorm(x, mean = mean(y), sd = sd(y)) * ifelse(freq == TRUE, yes = sum(!is.na(y)) * diff(h$breaks)[1], no = 1), add = TRUE, col = "red", lwd = 2, lty = 2, xpd = TRUE)
+  }
+  else curve(dnorm(x, mean = mean(y), sd = sd(y)) * ifelse(freq ==
+                                                             TRUE, yes = sum(!is.na(y)) * diff(h$breaks)[1], no = 1),
+             add = TRUE, col = "red", lwd = 2, lty = 2, xpd = TRUE)
 }
+
