8a1c8dce083be442b86658721a497ef4a7356c40
cugmas_990 <cugmas_990@f465408c-3b6e-4e74-ba7b-223a1f180679>
2021-04-23 13:24:15 +0000

    Popravljena je funkcija gen.network.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multius@51 f465408c-3b6e-4e74-ba7b-223a1f180679

--- pkg/nem/R/gen.network.R
+++ pkg/nem/R/gen.network.R
@@ -1,11 +1,25 @@
-gen.network <- function(type, errors, n = 40, symmetric = TRUE, loop = FALSE, size = "equal"){
+#' Generating binary netwrks with pre-specified leves of erros
+#'
+#' @description It generates the networks with a pre-specified levels of errors for each block.
+#' @param type A \code{matrix} containg types of blocks \code{"nul"}, \code{"null"} or \code{"com"}.
+#' @param errors A \code{matrix} containg levels of errors (between 0 and 1) for each block; a single value can be provided if all blocks are assumed to contain the same level of errors.
+#' @param n Network size.
+#' @param loop Set to \code{TRUE} to allow loops in a network or \code{FALSE} to forbid loops in a network.
+#' @param symmetric Should the network be symmetric \code{TRUE} or asymmetric \code{FALSE}.
+#' @param size Specify the size of each cluster; \code{"equal"} if all clusters are assumed to be of equal size; or specify the vector of sizes (the elements of the vector have sum to 1).
+#' @return A network with selected blockmodel type and level of errors.
+#' @details This function can be used to generate random networks with a given blockmodel type and specified block densities.
+#' @author Marjan Cugmas
+#' @export
+
+gen.network <- function(type, errors, n = 40, symmetric = FALSE, loop = FALSE, size = "equal"){
   new.network <- matrix(NA, nrow = n, ncol = n)
-  if (is.vector(size)==FALSE){
+  if (size == "equal"){
     n.for.each.block <- n/nrow(type)
     spodnje <- seq(from = 1, to = n, by = n.for.each.block)
     zgornje <- seq(from = 0, to = n, by = n.for.each.block)[-1]
   }
-  if (is.vector(size)==TRUE){
+  if (length(size) > 1){
     if (sum(size) != 1) warning("The size should sum to 1.")
     if (nrow(type) != length(size)) warning("The size of each block has to be specified.")
     n.for.each.block <- n*size
@@ -21,11 +35,11 @@ gen.network <- function(type, errors, n = 40, symmetric = TRUE, loop = FALSE, si
     for (j in 1:ncol(type)){
       if (type[i, j] == "com"){
         n.of.cells <- length(spodnje[i]:zgornje[i])*length(spodnje[j]:zgornje[j])
-        new.network[spodnje[i]:zgornje[i], spodnje[j]:zgornje[j]] <- matrix(sample(x = c(0, 1), size = n.of.cells, replace = T, prob = c(errors[i,j], 1 - errors[i,j])), nrow = length(spodnje[i]:zgornje[i]), ncol = length(spodnje[j]:zgornje[j]))
+        new.network[spodnje[i]:zgornje[i], spodnje[j]:zgornje[j]] <- matrix(sample(x = c(0, 1), size = n.of.cells, replace = TRUE, prob = c(errors[i,j], 1 - errors[i,j])), nrow = length(spodnje[i]:zgornje[i]), ncol = length(spodnje[j]:zgornje[j]))
       }
-      if (type[i, j] == "null"){
+      if (type[i, j] %in% c("null", "nul")){
         n.of.cells <- length(spodnje[i]:zgornje[i])*length(spodnje[j]:zgornje[j])
-        new.network[spodnje[i]:zgornje[i], spodnje[j]:zgornje[j]] <- matrix(sample(x = c(0, 1), size = n.of.cells, replace = T, prob = c(1 - errors[i,j], errors[i,j])), nrow = length(spodnje[i]:zgornje[i]), ncol = length(spodnje[j]:zgornje[j]))
+        new.network[spodnje[i]:zgornje[i], spodnje[j]:zgornje[j]] <- matrix(sample(x = c(0, 1), size = n.of.cells, replace = TRUE, prob = c(1 - errors[i,j], errors[i,j])), nrow = length(spodnje[i]:zgornje[i]), ncol = length(spodnje[j]:zgornje[j]))
       }
     }
   }
